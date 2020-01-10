610cf449bad57a3ab30a97e1b36cf71cce20e255
AJ <aj.cyclone@yahoo.com>
2020-01-10 16:22:55 +0000

    i have no idea what i'm doing

--- cypress/integration/accountPage.spec.js
+++ cypress/integration/accountPage.spec.js
@@ -1,15 +1,48 @@
-describe ("Account page has", () => {
-    
-    beforeEach ( () => {
-        cy.visit("/account?userId=1")
-    });
+import axios from "axios";
 
-    it ("header of Account Page", function() {
-        cy.get("h1").should("contain.text", "Account Page")
-    })
+describe("Account page", () => {
+  beforeEach(() => {
+    cy.visit("/account?userId=1");
+  });
 
-    it ("button labelled Back that takes user back to Home Page", function() {
-        cy.get("a.btn.btn-primary").should("contain.text", "Back")
-        
-    })
-})
+  it("has header of Account Page", () => {
+    cy.get("h1").should("contain.text", "Account Page");
+  });
+
+  it("has a button labelled Back that takes user back to Home Page", () => {
+    cy.get("a.btn.btn-primary").should("contain.text", "Back");
+  });
+
+  //   it("displays name", async () => {
+  //     const response = await axios.get(
+  //       "http://jsonplaceholder.typicode.com/users"
+  //     );
+  //     const users = response.data;
+
+  //     cy.get("tbody")
+  //       .children("tr")
+  //       .each((el, index) => {
+  //         const user = users[index];
+
+  //         cy.wrap(el)
+  //           .children("td")
+  //           .should("contain", `${user.name}`);
+  //       });
+  //   });
+
+  it("should have copyright for the current year should be displayed at the bottom", () => {
+    const currentYear = Cypress.moment().format("YYYY");
+    cy.contains("body", "© " + currentYear);
+  });
+});
+
+// 2. Page 2 should be the account page
+//     1. The account page should have the title "Account Page" √
+//     2. The account page should the following user information
+//         1. Name
+//         2. Email (Should be a mailto link)
+//         3. Phone
+//         4. Website (Should be a link to the site)
+//         5. A link to google maps with their location
+//     3. There should be a button labeled "Back" which takes the user back to the home page √
+// 3. A copyright for the current year should be displayed at the bottom of all pages √
--- cypress/integration/homePage.spec.js
+++ cypress/integration/homePage.spec.js
@@ -1,64 +1,71 @@
-import axios from 'axios'
+import axios from "axios";
 
-describe ('The home page', () => {
+describe("The home page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
 
-    beforeEach (() => {
-        cy.visit('/')
-    })
+  it("should have the title Home Page", () => {
+    cy.get("h1").should("contain.text", "Home Page");
+  });
 
-    it ('should have the title Home Page', () => {
-        cy.get('h1').should('contain.text', 'Home Page')
-    })
+  it("should display a welcome message ", () => {
+    cy.get("body").should("have.text", "Welcome");
+  });
 
-    it ('should display a welcome message ', () => {
-        cy.get('body').should('have.text', 'Welcome')
-    })
+  it("should display the users name and email address", async () => {
+    const response = await axios.get(
+      "http://jsonplaceholder.typicode.com/users"
+    );
+    const users = response.data;
 
-    it ('should display the users name and email address', async () => {
-        const response = await axios.get('http://jsonplaceholder.typicode.com/users')
-        const users = response.data
+    cy.get("tbody")
+      .children("tr")
+      .each((el, index, list) => {
+        const user = users[index];
 
-        cy.get('tbody')
-            .children('tr')
-            .each((el, index, list) => {
-                const user = users[index]
+        cy.wrap(el)
+          .children("td")
+          .should("contain", user.name);
 
-                cy.wrap(el)
-                    .children('td')
-                    .should('contain', user.name)
-                    
-                cy.wrap(el)
-                    .children('td')
-                    .should('contain', user.email)
-            })
-    })
+        cy.wrap(el)
+          .children("td")
+          .should("contain", user.email);
+      });
+  });
 
-        it ('should link to the account page for that specific user', async () => {
-            const response = await axios.get('http://jsonplaceholder.typicode.com/users')
+  it("should link to the account page for that specific user", async () => {
+    const response = await axios.get(
+      "http://jsonplaceholder.typicode.com/users"
+    );
+    const users = response.data;
 
-            const users = response.data
+    cy.get("tbody")
+      .children("tr")
+      .each((el, index, list) => {
+        const user = users[index];
 
-            cy.get('tbody')
-                .children('tr')
-                .each((el, index, list) => {
-                    const user = users[index]
+        cy.wrap(el)
+          .children("td>a")
+          .should("have.attr", "href", `/account?userId=${user.id}`);
+      });
+  });
 
-                    cy.wrap(el)
-                    .children('td')
-                    .children('a')
-                    .should('have.attr', 'href', `/account?userId=${user.id}`)
+  it("email should be a mailto link", () => {
+    cy.contains("tbody>tr>td", "@").should("have.attr", "href");
+  });
 
-                })    
-    })
-})
+  it("should have copyright for the current year should be displayed at the bottom", () => {
+    const currentYear = Cypress.moment().format("YYYY");
+    cy.contains("body", "© " + currentYear);
+  });
+});
 
-
-
-//1. Page 1 should be the home page 
+//1. Page 1 should be the home page
 //    1. The home page should have the title "Home Page" √
 //    2. The home page should display a welcome message √
-//    3. The home page should display a list of users from the users service 
+//    3. The home page should display a list of users from the users service
 //        1. Each user row should display the user's `name` and `email` address √
 //            1. The name should link to the account page for that specific user √
-//            2. The email should be a mailto link
+//            2. The email should be a mailto link √
 // 3. A copyright for the current year should be displayed at the bottom of all pages
